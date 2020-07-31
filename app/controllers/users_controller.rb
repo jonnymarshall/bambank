aabc2b5b6d0a98c8c507acaf91fe285e90677582
Jonny Marshall <jonnymarshall5@gmail.com>
2020-07-31 23:38:30 +0100

    user index serializer set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
+class UsersController < ApplicationController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   def index
     render json: UserSerializer.new(User.all)
   end
+  
   # GET /resource/sign_up
   # def new
   #   super
