c23b8a54d9ebb4e42790112318fba9dac0a9731c
Jonny Marshall <jonnymarshall5@gmail.com>
2020-08-01 14:17:17 +0100

    fixed buggy rendering of component -

--- app/javascript/packs/components/recent_transactions.component.jsx
+++ app/javascript/packs/components/recent_transactions.component.jsx
@@ -5,23 +5,28 @@ const RecentTransactions = (props) => {
   const { recentTransactions, payees, currentUserId } = props
   const findPayeeById = (id) => payees.find((payee) => payee.id === id)
 
+  const getRecentTransaction = (recentTransaction) => {
+    const currentUserIsSender = (recentTransaction.senderId == recentTransaction.currentUserId) ? true : false
+    const payee = currentUserIsSender ? findPayeeById(recentTransaction.receiverId) : findPayeeById(recentTransaction.senderId)
+    
+    return (payee) && <RecentTransaction
+      key={recentTransaction.id}
+      amount={recentTransaction.amount}
+      reference={recentTransaction.reference}
+      date={recentTransaction.createdAt.slice(0,10)}
+      senderId={recentTransaction.senderId}
+      receiverId={recentTransaction.receiverId}
+      currentUserId={currentUserId}
+      payee={payee}
+    />
+  }
+
   return (
     <section className="section">
       <div className="container">
         <h1 className="title is-5">Recent transactions</h1>
         <div className="columns is-multiline">
-          { recentTransactions && payees && recentTransactions.map(recentTransaction =>
-            <RecentTransaction
-              key={recentTransaction.id}
-              amount={recentTransaction.amount}
-              reference={recentTransaction.reference}
-              date={recentTransaction.createdAt.slice(0,10)}
-              senderId={recentTransaction.senderId}
-              receiverId={recentTransaction.receiverId}
-              currentUserId={currentUserId}
-              findPayeeById={findPayeeById.bind(this)}
-            />)
-          }
+          { recentTransactions && payees && recentTransactions.map(recentTransaction => { return getRecentTransaction(recentTransaction) })}
         </div>
       </div>
     </section>
